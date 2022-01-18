e6aca1a23cf266d0166882374dda36117ced6304
Dustin Ru <dustinr.dev@gmail.com>
2022-01-18 09:30:53 -0800

    Removed git symlink logic for compatibility

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -10,7 +10,7 @@ mkdir -p $DOTFILES_BACKUP
 local nvim_config_home=""~/.config/nvim""
 mkdir -p $nvim_config_home
 
-local folder_list=("git" "tmux" "zsh" "nvim")
+local folder_list=("tmux" "zsh" "nvim")
 for folder in ${folder_list[@]}; do
     for file in $(find -H "$DOTFILES_ROOT/$folder" -maxdepth 1 -mindepth 1 -printf "%P\n"); do
         
@@ -31,7 +31,6 @@ for folder in ${folder_list[@]}; do
 done
 
 # Manual symlink creation for now
-#ln -s "$DOTFILES_ROOT/git/.gitconfig"
 #ln -s "$DOTFILES_ROOT/tmux/.tmux.conf"
 #ln -s "$DOTFILES_ROOT/zsh/.zsh"
 #ln -s "$DOTFILES_ROOT/zsh/.p10k.zsh"
