3b3f3d5861ba064fbd49f8ac898194cb2235c1ae
Dustin Ru <dustin.ru@is.com>
2022-02-16 08:33:01 -0800

    Added ironSource aliases

--- zsh/.zsh/work.zsh
+++ zsh/.zsh/work.zsh
@@ -1,2 +1,10 @@
 # Work-specific aliases, zsh scripts, etc.
-#
+
+alias addsc='sudo ~/Downloads/addSE/addSecurityExceptions.sh'	# add network security config
+
+# Run PHP for TAM Encrypt/Decrypt Tool
+tamtool() {
+        cd $HOME/Documents/tam-utils/Curtis_Tool;
+        open http://localhost:8000;
+        php -S localhost:8000
+}
