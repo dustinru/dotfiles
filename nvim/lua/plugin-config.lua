1aac1669a5c4f2e7218aaa17df86ed4e647ac79e
Dustin Ru <dustinr.dev@gmail.com>
2022-12-08 00:43:53 -0800

    Fixed nvim-cmp update_capabilities deprecated error

--- nvim/lua/plugin-config.lua
+++ nvim/lua/plugin-config.lua
@@ -78,7 +78,7 @@ require("nvim-lsp-installer").setup({
   }
 })
 -- Setup lspconfig
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local servers_lsp = {'pyright','rust_analyzer','tsserver','eslint','sqls'}
 for _, lsp in ipairs(servers_lsp) do
   nvim_lsp[lsp].setup {
